5f97463de57ef398efe963276c941a0ef5c97e27
Piyawat10070400 <piyawat5930302600@gmail.com>
2025-10-26 14:48:54 +0700

    update cores

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://ma.family-sivarom.com"],
+  origin: [
+    "http://localhost:5173",
+    "https://ma.family-sivarom.com",
+    "https://app.family-sivarom.com",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
